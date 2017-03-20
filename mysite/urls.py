095ef7bdd8890eb2e75283c3f155111dcab05bb3
jinseungim <jinseungim@localhost.localdomain>
2017-03-20 17:28:44 +0900

    url modified

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,7 +4,8 @@ from django.contrib import admin
 
 
 urlpatterns = [
+
+    url(r'^admin/', include(admin.site.urls)),
     url(r'', include('blog.urls')),
-    url(r'^admin/', admin.site.urls),
 
 ]
